3d8434c926656f40c3a33f836e43b6e1a6de1413
Lucaman99 <1ceronijac@hdsb.ca>
2018-01-01 19:39:47 -0500

    Create init_db_names.js

---
+++ init_db_names.js
@@ -0,0 +1,26 @@
+var mysqldb = require('mysql')
+
+//Database names can be added to this array when it is determined how the information will be stored.
+var db_name_arr = []
+
+function init_db(db_name) {
+
+var connector = mysqldb.createConnection({host: "localhost", user: "root", password: "Lucaman"})
+connector.connect(function (err){
+	if (err) throw err;
+	var mysql = "CREATE DATABASE " + db_name
+	connector.query(mysql, function (err, data) {
+		if (err) throw err;
+	})
+	connector.end()
+})
+}
+if (db_name_arr.length > 0) {
+	for (var i = 0; i < db_name_arr.length; i++) {
+		init_db(db_name_arr[i])
+	}
+	console.log("Databases successfully created.")
+}
+else {
+	console.log("No databases to query.")
+}
