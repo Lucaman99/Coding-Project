e67eac89d3bf605bae82f02d0b2249fb209f23c7
Lucaman99 <1ceronijac@hdsb.ca>
2018-01-13 22:54:39 -0500

    Create receiver.js

---
+++ testing/receiver.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var app = express()
+var url = require('url')
+var http = require('http')
+var fs = require('fs')
+var mysql = require('mysql')
+
+var con = mysql.createConnection({host: 'localhost', user: 'root', password: 'Lucaman', database: "testing"})
+
+con.connect(function (err){
+	if (err) throw err;
+})
+
+function one() {
+
+	var sql = "SELECT * FROM test"
+   con.query(sql, function(err, data){
+   	if (err) throw err;
+
+
+   	console.log(decodeURI(data[data.length - 1].value2))
+   	var base = data.length
+
+   	function send() {
+	var sql = "SELECT * FROM test"
+   con.query(sql, function(err, data){
+   	if (err) throw err;
+   	if (base !== data.length) {
+   		console.log("New transmission")
+   		one()
+   	}
+   	else {
+   		setTimeout(send, 1000)
+   	}
+   })
+
+}
+send()
+
+   })
+
+}
+one()
